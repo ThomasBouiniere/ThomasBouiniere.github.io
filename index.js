3fb98f4fd335bd53ac209f9218f36f131b0e9abc
thomasBouiniere <thomas.bouiniere@gmail.com>
2022-08-04 21:04:18 +0200

    nouveau jeu

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 const counterDisplay = document.querySelector("h3");
+const levelDisplay = document.querySelector("span");
+const timer = document.querySelector("div");
 let counter = 0;
+let lvl = 1;
+let speed = 8;
+let temps = 30;
 
 const bubbleMaker = () => {
-  const bubble = document.createElement("span");
-  bubble.classList.add("bubble");
-  document.body.appendChild(bubble);
+  if (temps != 0) {
+    const bubble = document.createElement("span");
+    bubble.classList.add("bubble");
+    document.body.appendChild(bubble);
 
-  const size = Math.random() * 200 + 100 + "px";
-  bubble.style.height = size;
-  bubble.style.width = size;
+    const size = Math.random() * 200 + 100 + "px";
+    bubble.style.height = size;
+    bubble.style.width = size;
 
-  bubble.style.top = Math.random() * 100 + 50 + "%";
-  bubble.style.left = Math.random() * 100 + 50 + "%";
+    bubble.style.top = Math.random() * 100 + 120 + "%";
+    bubble.style.left = Math.random() * 100 + 120 + "%";
 
-  const plusMinus = Math.random() > 0.5 ? 1 : -1;
-  bubble.style.setProperty("--left", plusMinus * (Math.random() * 100) + "%");
+    const plusMinus = Math.random() > 0.5 ? 1 : -1;
+    bubble.style.setProperty("--left", plusMinus * (Math.random() * 100) + "%");
 
-  bubble.addEventListener("click", () => {
-    bubble.remove();
-    counter++;
-    counterDisplay.textContent = counter;
-  });
-  setTimeout(() => {
-    bubble.remove();
-  }, 8000);
+    bubble.style.setProperty("animation-duration", speed + "s");
+
+    bubble.addEventListener("click", () => {
+      bubble.remove();
+      counter++;
+      counterDisplay.textContent = counter;
+      if (counter % 10 == 0) {
+        lvl++;
+        levelDisplay.textContent = "Niveau " + lvl;
+        speed = speed * 0.75;
+        temps = 30;
+      }
+    });
+    setTimeout(() => {
+      bubble.remove();
+    }, 8000);
+  }
 };
 
+function diminuerTemps() {
+  timer.textContent = "Timer : " + temps;
+  if (temps != 0) {
+    temps--;
+  }
+}
+
+setInterval(diminuerTemps, 1000);
 setInterval(bubbleMaker, 1000);
